33fa35dfa0626a40c9a1640b16b8a0db0a209376
Billy Dentith <billydentith@hotmail.co.uk>
2024-11-26 23:15:32 +0000

    Adds required pages and sets up basic styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import MyEvents from './pages/MyEvents';
+import Account from './pages/Account';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-      <h1>Events Platform</h1>
-      <p>
-        Events platform template. 
-      </p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/my-events' element={<MyEvents />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
