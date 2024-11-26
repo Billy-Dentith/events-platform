bfe74f281835c98f02a0469bcd356f37202a098d
Billy Dentith <billydentith@hotmail.co.uk>
2024-11-26 21:43:44 +0000

    Adds basic frontend setup with vite/react

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Events Platform</h1>
+      <p>
+        Events platform template. 
+      </p>
+    </>
+  )
+}
+
+export default App;
