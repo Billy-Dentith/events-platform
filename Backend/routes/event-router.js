af47ecb187135c6835d0724d8c73c607793d1e33
Billy Dentith <billydentith@hotmail.co.uk>
2024-12-11 18:08:09 +0000

    Backend: Adds functionality to patch an event with schema validation

--- Backend/routes/event-router.js
+++ Backend/routes/event-router.js
@@ -1,10 +1,11 @@
-const { getEvents, joinEvent, addEvent, removeEvent } = require("../controllers/event.controllers");
+const { getEvents, joinEvent, addEvent, removeEvent, editEvent } = require("../controllers/event.controllers");
 
 const eventRouter = require("express").Router();
 
 eventRouter.get("/", getEvents);
 
 eventRouter.patch("/:event_id", joinEvent); 
+eventRouter.patch("/:event_id/edit", editEvent); 
 
 eventRouter.post("/", addEvent);
 
